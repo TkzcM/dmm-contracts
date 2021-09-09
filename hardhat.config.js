7c414db35d99faba5452775f254c0bb278b06b64
quang_neo <ducquang.kstn@gmail.com>
2021-09-09 13:33:30 +0700

    update env for matic, bsc and avax (#43)

--- hardhat.config.js
+++ hardhat.config.js
@@ -146,6 +146,7 @@ module.exports = {
 
 const INFURA_API_KEY = process.env.INFURA_API_KEY;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const MATIC_VIGIL_KEY = process.env.MATIC_VIGIL_KEY;
 
 if (INFURA_API_KEY != undefined && PRIVATE_KEY != undefined) {
   module.exports.networks.kovan = {
@@ -172,6 +173,43 @@ if (INFURA_API_KEY != undefined && PRIVATE_KEY != undefined) {
     timeout: 20000
   };
 }
+
+if (MATIC_VIGIL_KEY != undefined && PRIVATE_KEY != undefined) {
+  module.exports.networks.mumbai = {
+    url: `https://rpc-mumbai.maticvigil.com/v1/${MATIC_VIGIL_KEY}`,
+    accounts: [PRIVATE_KEY],
+    timeout: 20000
+  };
+  module.exports.networks.matic = {
+    url: `https://rpc-mainnet.maticvigil.com/v1/${MATIC_VIGIL_KEY}`,
+    accounts: [PRIVATE_KEY],
+    timeout: 20000
+  };
+}
+
+module.exports.networks.bsctestnet = {
+  url: `https://data-seed-prebsc-1-s2.binance.org:8545/`,
+  accounts: [PRIVATE_KEY],
+  timeout: 20000
+};
+
+module.exports.networks.bsc = {
+  url: `https://bsc-dataseed1.ninicoin.io/`,
+  accounts: [PRIVATE_KEY],
+  timeout: 20000
+};
+
+module.exports.networks.avaxtestnet = {
+  url: `https://api.avax-test.network/ext/bc/C/rpc`,
+  accounts: [PRIVATE_KEY],
+  timeout: 20000
+};
+
+module.exports.networks.avax = {
+  url: `https://api.avax.network/ext/bc/C/rpc`,
+  accounts: [PRIVATE_KEY],
+  timeout: 20000
+};
 
 if (process.env.ETHERSCAN_API_KEY != undefined) {
   module.exports.etherscan = {
